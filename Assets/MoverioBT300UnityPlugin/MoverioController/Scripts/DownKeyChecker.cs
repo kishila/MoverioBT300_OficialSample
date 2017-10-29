49d6cfb9dc4f703b00329f4307949d62cc945985
Nakai Tomoki <s16g459@stu.kagawa-u.ac.jp>
2017-10-29 09:54:09 +0900

    KeyEventを表示するように改良

---
+++ Assets/MoverioBT300UnityPlugin/MoverioController/Scripts/DownKeyChecker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using System;
+
+public class DownKeyChecker : MonoBehaviour {
+
+		public Text displayKeyEventValue;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Update () {
+			DownKeyCheck ();
+	}
+
+
+	void DownKeyCheck(){
+			if (Input.anyKeyDown) {
+					foreach (KeyCode code in Enum.GetValues(typeof(KeyCode))) {
+							if (Input.GetKeyDown (code)) {
+								displayKeyEventValue.text = code.ToString ();
+							}
+					}
+			}
+	}
+}
